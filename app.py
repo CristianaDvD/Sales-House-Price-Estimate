70da901bc0ee347e0a7e631088fc65853eba521d
Cristiana S David <cristiana.radu92@yahoo.com>
2025-11-09 06:04:43 +0000

    Add ML price estimate page

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_project_hypotheses import page_project_hypotheses_body
 from app_pages.page_correlation_study import page_correlation_study_body
+from app_pages.page_ml_price_estimate import page_ml_predict_price_body
 # Create an instance of the app 
 app = MultiPage(app_name= "House Sale Price Estimate")
 
@@ -12,6 +13,7 @@ app = MultiPage(app_name= "House Sale Price Estimate")
 app.add_page("👀 Project Overview", page_summary_body)
 app.add_page("✨ Hypotheses and Validation", page_project_hypotheses_body)
 app.add_page("📊 Correlation Study", page_correlation_study_body)
+app.add_page("🤖 Machine Learning Model", page_ml_predict_price_body)
 
 app.run()
 
